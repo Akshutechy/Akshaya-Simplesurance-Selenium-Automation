916a68bd1b688f660fbabf2bcf98ae32d68eed3e
Vignesh Srinivasa Raghavan <86973154+Akshutechy@users.noreply.github.com>
2023-12-14 23:08:33 +0100

    Added Explicit Wait Factory

--- Akshaya-Simplesurance-Assignment/src/main/java/pages/BasePage.java
+++ Akshaya-Simplesurance-Assignment/src/main/java/pages/BasePage.java
@@ -1,53 +1,22 @@
 package pages;
 
-import constants.FrameworkConstants;
 import driver.DriverManager;
 import enums.WaitStrategy;
+import factories.ExplicitWaitFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class BasePage {
 
     protected void sendKeys(By by, String valueToEnter, WaitStrategy waitStrategy){
-        if(waitStrategy==WaitStrategy.CLICKABLE){
-            waitForElementToBeClickable(by);
-        }
-        else if(waitStrategy==WaitStrategy.PRESENT){
-            waitForElementToBePresent(by);
-        }
-        DriverManager.getDriver().findElement(by).sendKeys(valueToEnter);
+        ExplicitWaitFactory.performExplicitWait(by,waitStrategy).sendKeys(valueToEnter);
     }
 
     protected void click(By by, WaitStrategy waitStrategy){
-        if(waitStrategy==WaitStrategy.CLICKABLE){
-            waitForElementToBeClickable(by);
-        }
-        else if(waitStrategy==WaitStrategy.PRESENT){
-            waitForElementToBePresent(by);
-        }
-        DriverManager.getDriver().findElement(by).click();
+        ExplicitWaitFactory.performExplicitWait(by,waitStrategy).click();
     }
 
-    protected void getText(By by, WaitStrategy waitStrategy){
-        if(waitStrategy==WaitStrategy.CLICKABLE){
-            waitForElementToBeClickable(by);
-        }
-        else if(waitStrategy==WaitStrategy.PRESENT){
-            waitForElementToBePresent(by);
-        }
-        DriverManager.getDriver().findElement(by).getText();
-    }
-
-    private void waitForElementToBeClickable(By by){
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(FrameworkConstants.getTimeout()));
-        wait.until(ExpectedConditions.elementToBeClickable(by));
-    }
-
-    private void waitForElementToBePresent(By by){
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(FrameworkConstants.getTimeout()));
-        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    protected String getText(By by, WaitStrategy waitStrategy){
+        ExplicitWaitFactory.performExplicitWait(by,waitStrategy);
+        return DriverManager.getDriver().findElement(by).getText();
     }
 }
