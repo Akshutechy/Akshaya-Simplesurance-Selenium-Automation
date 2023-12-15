f351d08d6ea34ef9aaf1f8bad8d2a902b335672f
Vignesh Srinivasa Raghavan <86973154+Akshutechy@users.noreply.github.com>
2023-12-15 01:11:08 +0100

    Cross Browser Testing Support

--- Akshaya-Simplesurance-Assignment/src/main/java/driver/Driver.java
+++ Akshaya-Simplesurance-Assignment/src/main/java/driver/Driver.java
@@ -4,6 +4,7 @@ import enums.ConfigProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import utils.ReadPropertyFile;
 
 import javax.swing.*;
@@ -12,10 +13,17 @@ import java.util.Objects;
 public final class Driver {
     private Driver(){}
 
+    private static WebDriver driver;
     public static void initDriver() throws Exception {
         if(Objects.isNull(DriverManager.getDriver())) {
-            WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver();
+            if(ReadPropertyFile.getValue(ConfigProperties.BROWSER).equalsIgnoreCase("chrome")){
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            else if(ReadPropertyFile.getValue(ConfigProperties.BROWSER).equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
             DriverManager.setDriver(driver);
             DriverManager.getDriver().manage().window().maximize();
             DriverManager.getDriver().get(ReadPropertyFile.getValue(ConfigProperties.URL));
